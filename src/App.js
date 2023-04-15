5ad46b31df618b2fd615b474368ece21e28d2240
Matvey Lapitchev <matvey.lapitchev@gmail.com>
2023-04-15 17:58:37 +0100

    eslint and pre-commit-hook has been added, file structure has been changed and necessary dependences has been installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <BrowserRouter />
   );
 }
 
