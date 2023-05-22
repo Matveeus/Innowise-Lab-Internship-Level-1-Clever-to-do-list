be6f9e0adde7b2d0a613782417534555a2c40a81
Matvey Lapitchev <matvey.lapitchev@gmail.com>
2023-05-22 14:53:35 +0200

    added a redirection loader to prevent access to the main page

---
+++ src/components/Loader.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container, Grid } from '@mui/material';
+import '../App.css';
+
+export default function Loader() {
+  return (
+    <Container>
+      <Grid
+        container
+        sx={{ height: window.innerHeight - 45 }}
+        alignItems="center"
+      >
+        <Grid
+          container
+          alignItems="center"
+          justifyContent="center"
+        >
+          <div className="lds-roller">
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../services/firebase';
 import Calendar from '../components/Calendar';
 import FooterButtons from '../components/FooterButtons';
 import AllTasks from '../components/task/AllTasks';
+import Loader from '../components/Loader';
 
 function HomePage() {
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
+      setIsLoading(false);
       if (!user) {
         navigate('/login');
       }
     });
   });
+
+  if (isLoading) {
+    return <Loader />;
+  }
   return (
     <>
       <Calendar />
