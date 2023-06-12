ce6e24956c1c239640a4ce956ec6522538bc8eaf
Matvey Lapitchev <matvey.lapitchev@gmail.com>
2023-06-12 11:59:37 +0200

    Login & Register components refactored

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,18 +1,15 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../services/firebase';
 import Form from './Form';
 import useAuthState from '../../hooks/useAuthState';
 
 function Login() {
-  const navigate = useNavigate();
   const [error, setError] = useState(null);
 
   const handleLogin = async (email, password) => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      navigate('/');
     } catch (err) {
       setError(err.message);
     }
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../services/firebase';
 import Form from './Form';
 import useAuthState from '../../hooks/useAuthState';
 
 function Register() {
-  const navigate = useNavigate();
   const [error, setError] = useState(null);
 
   const handleRegister = async (email, password, passwordRepeat) => {
     if (password === passwordRepeat) {
       try {
         await createUserWithEmailAndPassword(auth, email, password);
-        navigate('/');
       } catch (err) {
         setError(err.message);
       }
